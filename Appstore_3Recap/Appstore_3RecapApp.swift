8e5f0070518edab08d5635da576fa145ef2bd024
woosuk <kevincool6@naver.com>
2025-04-24 21:49:40 +0900

    Initial Commit

---
+++ Appstore_3Recap/Appstore_3RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Appstore_3RecapApp.swift
+//  Appstore_3Recap
+//
+//  Created by 권우석 on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Appstore_3RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appstore_3Recap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appstore_3Recap
+//
+//  Created by 권우석 on 4/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
