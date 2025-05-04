e90d769b07886a1e06f4dd3a7994b4f385dc4c26
woosuk <kevincool6@naver.com>
2025-05-04 16:00:19 +0900

    refactor: 코드 수정

--- Appstore_3Recap/App/Appstore_3RecapApp.swift
+++ Appstore_3Recap/App/Appstore_3RecapApp.swift
@@ -12,8 +12,7 @@ struct Appstore_3RecapApp: App {
     // 앱이 시작될 때 NetworkMonitor 초기화
     init() {
         // 네트워크 모니터링 시작
-        let _ = NetworkMonitor.shared //  앱딜리게이트 대신 역할을 하는 곳에서 관찰을 시작하도록 구현
-        
+        let _ = NetworkMonitor.shared 
         // 앱 시작 시 다운로드 매니저 설정 추가
         AppDownloadManager.shared.setupOnAppLaunch()
     }
